d15f20805ca428986e2312073c264eab62dc1e3a
inshyDC <1971553185@qq.com>
2019-09-16 19:07:19 +0800

    模型1

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * Created by wbl
+ * on 2019/9/16 18:50
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hellofgfh  !!!");
+    }
+}
